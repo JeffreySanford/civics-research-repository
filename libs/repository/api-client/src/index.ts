730fa59be6728d6a673e07a8b328eb6ae126d355
Jeffrey Sanford <jeffrey_sanford@hotmail.com>
2026-08-29 18:45:19 -0500

    Export corpus storage API client

--- libs/repository/api-client/src/index.ts
+++ libs/repository/api-client/src/index.ts
@@ -1,4 +1,5 @@
 export * from './lib/repository-api-client';
 export * from './lib/repository-error';
 export * from './lib/search-comparison-api';
+export * from './lib/corpus-storage-api';
 export type * from './generated/repository-api.types';
