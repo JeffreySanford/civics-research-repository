991881cd7cd4bdd72d8b80495727a581cbb7d0ce
Jeffrey Sanford <jeffrey_sanford@hotmail.com>
2026-08-29 12:38:04 -0500

    Export search comparison API client

--- libs/repository/api-client/src/index.ts
+++ libs/repository/api-client/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/repository-api-client';
 export * from './lib/repository-error';
+export * from './lib/search-comparison-api';
 export type * from './generated/repository-api.types';
