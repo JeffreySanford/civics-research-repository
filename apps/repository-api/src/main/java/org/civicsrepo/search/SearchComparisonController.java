9dfc66cdbeaabf42f63b37d7604fc439d2012dbb
Jeffrey Sanford <jeffrey_sanford@hotmail.com>
2026-08-29 12:37:36 -0500

    Expose search comparison endpoints

---
+++ apps/repository-api/src/main/java/org/civicsrepo/search/SearchComparisonController.java
@@ -0,0 +1,31 @@
+package org.civicsrepo.search;
+
+import java.util.List;
+import org.civicsrepo.generated.dto.SearchComparisonRequest;
+import org.civicsrepo.generated.dto.SearchComparisonResponse;
+import org.civicsrepo.generated.dto.SearchComparisonScenario;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/search/comparison")
+public class SearchComparisonController {
+    private final SearchComparisonService searchComparisonService;
+
+    public SearchComparisonController(SearchComparisonService searchComparisonService) {
+        this.searchComparisonService = searchComparisonService;
+    }
+
+    @GetMapping("/scenarios")
+    public List<SearchComparisonScenario> scenarios() {
+        return searchComparisonService.scenarios();
+    }
+
+    @PostMapping("/run")
+    public SearchComparisonResponse run(@RequestBody SearchComparisonRequest request) {
+        return searchComparisonService.run(request);
+    }
+}
