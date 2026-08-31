399cc1ab51b61acaec369e71b235344f8599fc33
Jeffrey Sanford <jeffrey_sanford@hotmail.com>
2026-08-31 18:56:50 -0500

    fix: include Jakarta transaction annotation API

--- apps/repository-api/build.gradle.kts
+++ apps/repository-api/build.gradle.kts
@@ -100,6 +100,7 @@ tasks.register("resolveDependencies") {
 
 dependencies {
     implementation("com.fasterxml.jackson.core:jackson-databind")
+    implementation("jakarta.transaction:jakarta.transaction-api")
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-webmvc")
