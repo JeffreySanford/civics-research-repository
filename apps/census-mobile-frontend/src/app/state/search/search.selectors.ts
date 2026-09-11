c33ba02d3061dd2622c99feda56e4796f5ed042a
Jeffrey Sanford <jeffrey_sanford@hotmail.com>
2026-09-11 13:41:39 -0500

    feat: add mobile search selectors

---
+++ apps/census-mobile-frontend/src/app/state/search/search.selectors.ts
@@ -0,0 +1,20 @@
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import type { MobileSearchState } from './search.reducer';
+
+export const selectMobileSearchState =
+  createFeatureSelector<MobileSearchState>('mobileSearch');
+
+export const selectMobileSearchResponse = createSelector(
+  selectMobileSearchState,
+  (state) => state.response,
+);
+
+export const selectMobileSearchLoading = createSelector(
+  selectMobileSearchState,
+  (state) => state.loading,
+);
+
+export const selectMobileSearchError = createSelector(
+  selectMobileSearchState,
+  (state) => state.error,
+);
