57885e737ed5b8efa09c2e6d6bf6510be4b5de73
Jeffrey Sanford <jeffrey_sanford@hotmail.com>
2026-09-11 13:41:19 -0500

    feat: add mobile search state actions

---
+++ apps/census-mobile-frontend/src/app/state/search/search.actions.ts
@@ -0,0 +1,11 @@
+import { createActionGroup, props } from '@ngrx/store';
+import type { SearchQuery, SearchResponse } from 'repository-api-client';
+
+export const MobileSearchActions = createActionGroup({
+  source: 'Mobile Repository Search',
+  events: {
+    'Search Submitted': props<{ query: SearchQuery }>(),
+    'Search Loaded': props<{ response: SearchResponse }>(),
+    'Search Failed': props<{ message: string }>(),
+  },
+});
