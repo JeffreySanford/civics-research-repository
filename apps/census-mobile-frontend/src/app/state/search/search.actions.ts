f457d04286e8b28cd6aba2818207d6ca476ab93e
Jeffrey Sanford <jeffrey_sanford@hotmail.com>
2026-09-11 14:15:55 -0500

    feat: add mobile search page requests

--- apps/census-mobile-frontend/src/app/state/search/search.actions.ts
+++ apps/census-mobile-frontend/src/app/state/search/search.actions.ts
@@ -5,6 +5,7 @@ export const MobileSearchActions = createActionGroup({
   source: 'Mobile Repository Search',
   events: {
     'Search Submitted': props<{ query: SearchQuery }>(),
+    'Page Requested': props<{ page: number }>(),
     'Search Loaded': props<{ response: SearchResponse }>(),
     'Search Failed': props<{ message: string }>(),
   },
