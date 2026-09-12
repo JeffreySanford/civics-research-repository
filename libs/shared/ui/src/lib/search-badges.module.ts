f5b0245393b71f73ec3371f02a41e310f0ec10cc
Jeffrey Sanford <jeffrey_sanford@hotmail.com>
2026-09-12 10:29:02 -0500

    feat(ui): expose explainability with shared search evidence

--- libs/shared/ui/src/lib/search-badges.module.ts
+++ libs/shared/ui/src/lib/search-badges.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
+import { SearchExplainabilityDialogComponent } from './search-explainability-dialog/search-explainability-dialog.component';
 import { SearchRankBadgeComponent } from './search-rank-badge/search-rank-badge.component';
 import { SearchRelevanceBadgeComponent } from './search-relevance-badge/search-relevance-badge.component';
 
 @NgModule({
-  imports: [SearchRankBadgeComponent, SearchRelevanceBadgeComponent],
-  exports: [SearchRankBadgeComponent, SearchRelevanceBadgeComponent],
+  imports: [
+    SearchRankBadgeComponent,
+    SearchRelevanceBadgeComponent,
+    SearchExplainabilityDialogComponent,
+  ],
+  exports: [
+    SearchRankBadgeComponent,
+    SearchRelevanceBadgeComponent,
+    SearchExplainabilityDialogComponent,
+  ],
 })
 export class SearchBadgesModule {}
